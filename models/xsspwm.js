df9d87d1e8ea46092dd6d9a277bde3d3efe9cb2c
root <root@localhost.localdomain>
2015-09-15 00:58:24 +0200

    lunedi1

--- models/xsspwm.js
+++ models/xsspwm.js
@@ -2,18 +2,13 @@ var mongoose = require('mongoose');
 
 
 var TestCase = new mongoose.Schema({
-ID_TestCase: Number,
+
+ID_TestCase:String,
 txt_nomeFile: String,
-lastUpdate: Date
+lastUpdate:Date
 
 });
+
 module.exports = mongoose.model('TestCase', TestCase);
 
-var URLs = new mongoose.Schema({
-ID_URL: Number,
-ID_TestCase: Number,
-txtURL: String
 
-});
-
-module.exports = mongoose.model('URLs', URLs);
--- routes/RouterTestCase.js
+++ routes/RouterTestCase.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 var mongoose = require('mongoose');
-var TestCase = require('../models/xsspwm');
+var TestCase = require('../models/xsspwm.js');
 
 /* GET /games listing. */
 router.get('/', function(req, res, next) {
