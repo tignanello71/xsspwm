65b3981dca0abefc4ca294c26823329b50a1870a
root <root@localhost.localdomain>
2015-09-14 23:55:06 +0200

    lunedi

--- web.js
+++ web.js
@@ -12,6 +12,14 @@ mongoose.connect(process.env.OPENSHIFT_MONGODB_DB_URL || "mongodb://127.0.0.1:27
     }
 });
 
+// cors
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
 app.use(bodyParser());
 
 // In questo modo posso fare delle mini-app, 
